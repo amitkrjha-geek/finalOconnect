eb3bd3ffd94ec44aaaa03a62add871213219f04a
amit kumar jha <amit.kumarjha.met19@itnhu.ac.in>
2021-08-14 13:31:10 +0530

    postschema done

--- index.js
+++ index.js
@@ -5,7 +5,26 @@ const morgan=require('morgan');
 const bodyParser=require('body-parser');
 
 
+//app.use(bodyParser.json());
+app.use(express.json()); 
+app.use(express.urlencoded());
 
+
+
+// handling of CORS Error
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
+
+//mongodb connection
 mongoose.connect('mongodb://dboconnect:dboconnect2021@cluster0-shard-00-00.etqtv.mongodb.net:27017,cluster0-shard-00-01.etqtv.mongodb.net:27017,cluster0-shard-00-02.etqtv.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-1b6voy-shard-0&authSource=admin&retryWrites=true&w=majority').then(
     result=>{
     app.listen(3000);
---
+++ models/post.js
@@ -0,0 +1,68 @@
+const mongoose=require('mongoose');
+
+const Schema=mongoose.Schema;
+
+const postSchema=new Schema({
+    creator:{
+        type:Object,
+        required:String
+    },
+    companyname:{
+        type:String,
+        required:true
+    },
+    linkedlnurl:{
+        type:String,
+        required:true
+    },
+    websiteurl:{
+        type:String
+    },
+    mode:{
+        type:String,
+        required:true
+    },
+    startdate:{
+        type:String,
+        required:true
+    },
+    Duration:{
+        type:String,
+        required:true
+    },
+    stipend:{
+        type:String,
+        
+    },
+    applyby:{
+        type:String,
+        required:true
+    },
+    field:{
+        type:String,
+        required:true
+    },
+    wca:{
+        type:String,
+        required:true
+    },
+    skillreq:{
+        type:String,
+        required:true
+    },
+    perks:{
+        type:String,
+        required:true
+    },
+    q1:{
+        type:String,
+        required:true
+    },
+    q2:{
+        type:String,
+        required:true
+    }
+},
+{timestamps:true}
+);
+module.exports=mongoose.model('post',postSchema);

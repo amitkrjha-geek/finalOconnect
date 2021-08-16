b244504517cdd99bf578a352007a32bcc4cf405d
amit kumar jha <amit.kumarjha.met19@itnhu.ac.in>
2021-08-16 21:46:46 +0530

    slight change in response schema

--- models/post.js
+++ models/post.js
@@ -7,8 +7,8 @@ const postSchema = new Schema({
     userRespondedToPost: [
         {
             type: Schema.Types.ObjectId,
-            ref: 'User',
-            default:[],
+            ref: 'Response',
+        
         }
     ],
     companyname: {
